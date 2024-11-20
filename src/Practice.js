fa2a4f6e044428deb6bcbcb966a71c71f51da7d3
Dmytro Sautin <dimasautin8@gmail.com>
2024-11-20 09:40:18 +0100

    Second

--- src/Practice.js
+++ src/Practice.js
@@ -1,15 +1,33 @@
 import './Practice.css'
-
+import logo from './logo2.svg'
+import close from './close.png'
 
 const Practice = () => {
     return (
         <header>
             <div class="header-left">
                 <a href='localhost:3000'>
-                    <img src='src/logo.svg' alt='Friskay Logo'/>
+                    <img width='17%' src={logo} alt='Friskay Logo'/>
                     <div class="devider"></div>
                 </a>
             </div>
+            
+            <nav>
+                <a href='#' class='menu-open hide-desktop'>
+                    <img src={logo} width='17%'></img>
+                </a>  
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img width='17%' src={close} alt="close menu" />
+                        </a>
+                        <li><a href="#">Our food</a></li>
+                        <li><a href="#">Ingredients</a></li>
+                        <li><a href="#">Studies</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
